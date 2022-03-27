1d4532230b416f8b6edbdb79c2eb93a6154440a7
Zalán Bálint Lévai <zalan.levai@gmail.com>
2022-03-27 03:31:50 +0100

    feat(nether_wait): Use Nether Portals in survival without delay
    Teleporting with Nether Portals in creative is instant, but a significant delay is incurred in survival. Internally, the portal wait time is tied to the player's invulnerability property. InstantNetherPortals, an earlier plugin for the same purpose removes the delay in survival by making the player invulnerable for an instant. This plugin uses the same workaround.
    
    For the workaround to work, invulnerability must be given through the net.minecraft.server/CraftBukkit API. The downside is that CraftBukkit is a versioned package accessible through `org.bukkit.craftbukkit.<VERSION>`, which must be hardcoded (instead of being dynamically resolved based on the build environment). This may be worked around at some point later.
    
    In addition to the functionality of the original plugin, teleportation happens after the normal delay when crouching in the Nether Portal in survival.

--- plugins/nether_wait/src/main/kotlin/outer_space/nether_wait/Plugin.kt
+++ plugins/nether_wait/src/main/kotlin/outer_space/nether_wait/Plugin.kt
@@ -1,13 +1,40 @@
 package outer_space.nether_wait
 
+import org.bukkit.GameMode
+import org.bukkit.Material
+import org.bukkit.craftbukkit.v1_18_R2.entity.CraftPlayer
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener as EventListener
+import org.bukkit.event.player.PlayerMoveEvent
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitRunnable
 
 class Plugin: JavaPlugin() {
     override fun onEnable() {
-        this.logger.info("[outer_space::nether_wait] enabled")
+        this.server.pluginManager.registerEvents(PlayerMoveListenerImpl(this), this)
     }
+}
 
-    override fun onDisable() {
-        this.logger.info("[outer_space::nether_wait] disabled")
+class PlayerMoveListenerImpl(
+    private val plugin: Plugin,
+): EventListener {
+    @EventHandler
+    fun onPlayerMove(event: PlayerMoveEvent) {
+        val player = event.player as CraftPlayer
+        val playerHandle = player.handle
+
+        if (player.gameMode == GameMode.CREATIVE) return
+        if (player.location.block.type != Material.NETHER_PORTAL) return
+        if (playerHandle.isCrouching) return
+
+        playerHandle.abilities.invulnerable = true
+        playerHandle.onUpdateAbilities()
+
+        object: BukkitRunnable() {
+            override fun run() {
+                playerHandle.abilities.invulnerable = false
+                playerHandle.onUpdateAbilities()
+            }
+        }.runTaskLater(this.plugin, 3)
     }
 }
